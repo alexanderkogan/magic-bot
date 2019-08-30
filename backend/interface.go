a3b8670d2d8808d93514951049b807db4494dde3
Alexander Kogan <alexander.kogan@itemis.com>
2019-08-30 13:48:01 +0200

    add battlefield zone with card representations

--- backend/interface.go
+++ backend/interface.go
@@ -8,6 +8,10 @@ type NewGameRequest struct {
 type Battlefield struct {
 	You   Player
 	Enemy Player
+	Zone  Zones
+}
+type Zones struct {
+	Battlefield BattlefieldZone
 }
 
 type Player struct {
@@ -17,6 +21,40 @@ type Player struct {
 }
 
 type DeckID string
+
+type BattlefieldZone struct {
+	You   []Card
+	Enemy []Card
+}
+
+type Card struct {
+	ID     CardID
+	Tapped bool
+}
+
+type CardID struct {
+	Set              SetID
+	CollectorsNumber int
+}
+
+type SetID string
+
+// Standard until Autumn 2019
+const (
+	Ixalan                              SetID = "XLN"
+	RivalsOfIxalan                            = "RIX"
+	Dominaria                                 = "DOM"
+	GlobalSeriesJiangYangguAndMuYanling       = "GS1"
+	CoreSet2019                               = "M19"
+)
+
+// Standard until Autumn 2020
+const (
+	GuildsOfRavnica   SetID = "GRN"
+	RavnicaAllegiance       = "RNA"
+	WarOfTheSpark           = "WAR"
+	CoreSet2020             = "M20"
+)
 
 type Server interface {
 	NewGame(NewGameRequest) Battlefield
